3c3f423291ce18850f7ddf3aecf0bb8d562c4194
hobiJeong <137868493+hobiJeong@users.noreply.github.com>
2024-11-15 15:52:55 +0900

    [level 1] Title: 모의고사, Time: 17.34 ms, Memory: 39.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,31 @@
+function solution(answers) {
+  arr1 = [1, 2, 3, 4, 5];
+  arr2 = [2, 1, 2, 3, 2, 4, 2, 5];
+  arr3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+  scores = [];
+  answer = [];
+  function grading(arr, answer) {
+    let answerCount = 0;
+    let c = 0;
+    while (answer.length > arr.length) {
+      arr = arr.concat(arr);
+      
+    console.log(arr);
+    }
+    for (let i = 0; i < answer.length; i++) {
+      if (arr[i] === answer[i]) {
+        answerCount += 1;
+      }
+    }
+    scores.push(answerCount);
+  }
+  grading(arr1,answers);
+  grading(arr2,answers);
+  grading(arr3,answers);
+  for (let i = 0; i < scores.length; i++) {
+    if (scores[i] === Math.max(...scores)) {
+      answer.push(i + 1);
+    }
+  }
+  return answer;
+}
