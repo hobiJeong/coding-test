94d087de51eee9e168a717a8cbb0617c684a2e64
hobiJeong <137868493+hobiJeong@users.noreply.github.com>
2024-11-15 15:58:23 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.55 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,39 +1,6 @@
 function solution(wallpaper) {
-    const [maxY, maxX] = [wallpaper.length - 1, wallpaper[0].length - 1];
+    console.log(wallpaper);
     
-    let firstFileIndex;
-    let lastFileIndex;
-    
-    for (let i = 0; i < wallpaper.length; i++) {
-        if (wallpaper[i].indexOf('#') >= 0) {
-            firstFileIndex = i;
-            
-            break;
-        }
-    }
-    
-    for (let i = maxY; i >= 0; i--) {
-        if (wallpaper[i].indexOf('#') >= 0) {
-            lastFileIndex = i + 1;
-            
-            break;
-        }
-    }
-    
-    const fileIndexSet = new Set();
-    
-    wallpaper.forEach((el) => {
-        for (i = 0; i <= maxX; i++) {
-            if (el[i] === '#') {
-                fileIndexSet.add(i);
-            }
-        }
-    });
-    
-    const [minIndex, maxIndex] = [
-        Math.min(...fileIndexSet),
-        Math.max(...fileIndexSet) + 1
-    ];
-    
-    return [firstFileIndex, minIndex, lastFileIndex, maxIndex];
+    var answer = [];
+    return answer;
 }
