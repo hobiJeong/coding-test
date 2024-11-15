533553a77884dd1db642c5b7bc061d62ead7eda6
hobiJeong <137868493+hobiJeong@users.noreply.github.com>
2024-11-15 15:44:23 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.js
@@ -1,16 +1,11 @@
 function solution(a, d, included) {
     let sum = 0;
+    let b;
     
     for (let i = 0; i < included.length; i++) {
-        if (included[i]) {
-            if (i === 0) {
-                sum += a;
-                
-                continue;
-            }
-            
-            sum += a + d * i;
-        }
+        included[i] && i === 0 ? 
+            sum += a : included[i] && i !== 0 ? 
+            sum += a + d * i : b = 'ab';
     }
     
     return sum;
